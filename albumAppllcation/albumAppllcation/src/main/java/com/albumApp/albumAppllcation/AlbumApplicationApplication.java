251a7c3c6a33e5a5d3f07cf208b146f4423b283e
AbinayaRav <abirav26@gmail.com>
2020-12-21 04:11:54 -0500

    Committing all the Java files

---
+++ albumAppllcation/albumAppllcation/src/main/java/com/albumApp/albumAppllcation/AlbumApplicationApplication.java
@@ -0,0 +1,13 @@
+package com.albumApp.albumAppllcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumApplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApplicationApplication.class, args);
+	}
+
+}
---
+++ albumAppllcation/albumAppllcation/src/test/java/com/albumApp/albumAppllcation/AlbumAppllcationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albumApp.albumAppllcation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumAppllcationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
